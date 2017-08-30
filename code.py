2499337ff8a1daca8eaec79dfa18f77b3f6fc5ba
cdeepika <31205315+cdeepika@users.noreply.github.com>
2017-08-30 21:48:58 +0530

    Add files via upload
    Assignment Submission

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ This is due on 30th August 2017.
 """
 ##################################################
 #Complete the functions as specified by docstrings
-
+# Coded in Python 3
 
 # 1
 
@@ -18,10 +18,11 @@ def entries_less_than_ten(L):
     Returns:
         A sublist of L consisting of those entries which are less than 10.
     """
-    return #Add your code here
+    return [i for i in L if  i< 10]
 
 #Test
 #print entries_less_than_ten([2, 13, 4, 66, -5]) == [2, 4, 6, -5]
+# This test has a spelling mistake. Assuming it is 6 and not 66. - Deepika 
 
 # 2
 
@@ -35,7 +36,9 @@ def number_of_negatives(L):
     Returns:
         number of entries of L which are negative
     """
-    pass ##YOUR CODE REPLACES THIS
+    #same logic as #1
+    l = [i for i in L if  i< 0]
+    return len(l)
 
 # TEST
 #print number_of_negatives([2, -1, 3, 0, -1, 0, -45, 21]) == 3
@@ -53,7 +56,7 @@ def common_elements(L1, L2):
         A list whose elements are the common elements of ``L1`` and
         ``L2`` WITHOUT DUPLICATES.
     """
-    pass # your code goes here
+    return list(set([i for i in L1 if i in L2]))
 
 #TEST
 #common_elements([1, 2, 1, 4, "bio", 6, 1], [4, 4, 2, 1, 3, 5]) == [1, 2, 4]
@@ -66,10 +69,15 @@ def fibonacci_generator():
     The Fibonacci sequence 1, 1, 2, 3, 5, 8, 13, 21,...
     is defined by a1=1, a2=1, and an = a(n-1) + a(n-2).
     """
-    pass # Hint: use the ``yield`` command.
-
+    a1 = 1
+    a2 = 1
+    while True:
+        yield a1
+        a1, a2 = a2, a1 + a2
 #TEST
 #f = fibonacci()
+# Use f.__next__() for printing. It gives individual elements but doesn't fit in the test provided. 
+#Throws AttributeError.
 #[f.next() for f in range(10)] == [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #5
@@ -77,8 +85,9 @@ def largest_fibonacci_before(n):
     """
     Return the largest Fibonacci number less than ``n``.
     """
-    pass #Your code goes here.
-
+    #read output of above generator upto ``n`` into a list ``l`` and print the preceding index.
+    #len(l) -1 => index of ``n``
+    return l[len(l)-2] 
 #TEST
 #largest-fibonacci_before(55) == 34
 
@@ -94,10 +103,21 @@ def catalan_generator():
 #TEST
 #c = catalan_generator()
 #[c.next() for i in range(10)] == [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862]
-
-    
-    
+ 
 #7
 ### CREATE YOUR OWN FUNCTION. Make sure it has a nice docstring.
 # See https://www.python.org/dev/peps/pep-0257/
 # for basic tips on docstrings.
+def fibonacci(n):
+    """
+    Returns a list of fibonacci series of length ''n''.
+    """
+    l=[]
+    x,y = 0,1
+    for i in range(n):
+        n = x+y
+        l.append(n)
+        x,y = y,y+1
+    return l
+##TEST
+#fibonacci(5) == [1, 3, 5, 7, 9]
